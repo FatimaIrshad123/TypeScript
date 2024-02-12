46b93eeb3425a80d3f3701a3161dfdddf95ca682
FatimaIrshad <bibimemoona2017@gmail.com>
2024-02-12 11:08:53 +0500

    Enum practice code

---
+++ enum.ts
@@ -0,0 +1,14 @@
+
+enum Direction {
+    Up = 0,
+    Down,
+    Right,
+    Left
+}
+// console.log(Direction.Right)
+function doSomething(keyPressed : Direction){
+    
+}
+doSomething(Direction.Right)
+console.log(Direction.Down)
+console.log(Direction.Up)
